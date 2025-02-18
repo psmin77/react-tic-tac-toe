0fed2959efcb9b3008e4410940ccf3de5ea93b1e
Seongmin <chr92@naver.com>
2025-02-18 10:23:10 +0900

    feat: 7. Rendering Lists - challenge2

--- describing-the-ui/src/App.js
+++ describing-the-ui/src/App.js
@@ -1,32 +1,19 @@
-import { people } from "./data.js";
-import { getImageUrl } from "./utils.js";
+import { recipes } from "./data.js";
 
-function Item({ person }) {
+export default function RecipeList() {
   return (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {" " + person.profession + " "}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
-}
-
-export default function List() {
-  const chemists = people
-    .filter((person) => person.profession === "chemist")
-    .map((person) => <Item key={person.id} person={person} />);
-  const others = people
-    .filter((person) => person.profession !== "chemist")
-    .map((person) => <Item key={person.id} person={person} />);
-
-  return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{chemists}</ul>
-      <ul>{others}</ul>
-    </article>
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map((i) => (
+              <li key={i}>{i}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
   );
 }
