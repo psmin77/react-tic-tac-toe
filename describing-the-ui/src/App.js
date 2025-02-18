dc2be5865f572aed26286d6b692d08b60bea7b2a
Seongmin <chr92@naver.com>
2025-02-18 10:27:28 +0900

    feat: 7. Rendering Lists - challenge3

--- describing-the-ui/src/App.js
+++ describing-the-ui/src/App.js
@@ -1,18 +1,24 @@
 import { recipes } from "./data.js";
 
+function Recipe({ id, name, ingredients }) {
+  return (
+    <div key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((i) => (
+          <li key={i}>{i}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((i) => (
-              <li key={i}>{i}</li>
-            ))}
-          </ul>
-        </div>
+        <Recipe key={recipe.id} {...recipe} />
       ))}
     </div>
   );
