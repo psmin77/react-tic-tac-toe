acd1793acb25bec1ab4b3ec0c2918f67b8b5c480
Seongmin <chr92@naver.com>
2025-02-20 10:37:48 +0900

    feat: 2-2. State a components memory - challenge1

--- adding-interactivity/src/App.js
+++ adding-interactivity/src/App.js
@@ -1,16 +1,46 @@
-export default function LightSwitch() {
-  function handleClick() {
-    let bodyStyle = document.body.style;
-    if (bodyStyle.backgroundColor === 'black') {
-      bodyStyle.backgroundColor = 'white';
-    } else {
-      bodyStyle.backgroundColor = 'black';
-    }
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handlePrevClick() {
+    setIndex(index - 1);
   }
 
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  const isDisabledPrev = index === 0;
+  const isDisabledNext = index === sculptureList.length - 1;
+
+  let sculpture = sculptureList[index];
   return (
-    <button onClick={handleClick}>
-      Toggle the lights
-    </button>
+    <>
+      <button onClick={handlePrevClick} disabled={isDisabledPrev}>
+        Prev
+      </button>
+      <button onClick={handleNextClick} disabled={isDisabledNext}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   );
 }
