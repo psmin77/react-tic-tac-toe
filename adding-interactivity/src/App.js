725c7b28bba427f9f991b26181ff4839aefbe36e
Seongmin <chr92@naver.com>
2025-02-21 09:53:57 +0900

    feat: 2-4. State as a snapshot - challenge1

--- adding-interactivity/src/App.js
+++ adding-interactivity/src/App.js
@@ -1,46 +1,23 @@
 import { useState } from "react";
-import { sculptureList } from "./data.js";
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
 
-  function handlePrevClick() {
-    setIndex(index - 1);
+  function handleClick() {
+    setWalk(!walk);
+    alert(walk ? "다음은 정지입니다." : "다음은 걷기입니다.");
   }
 
-  function handleNextClick() {
-    setIndex(index + 1);
-  }
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  const isDisabledPrev = index === 0;
-  const isDisabledNext = index === sculptureList.length - 1;
-
-  let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handlePrevClick} disabled={isDisabledPrev}>
-        Prev
-      </button>
-      <button onClick={handleNextClick} disabled={isDisabledNext}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
+      <button onClick={handleClick}>Change to {walk ? "Stop" : "Walk"}</button>
+      <h1
+        style={{
+          color: walk ? "darkgreen" : "darkred",
+        }}
+      >
+        {walk ? "Walk" : "Stop"}
+      </h1>
     </>
   );
 }
